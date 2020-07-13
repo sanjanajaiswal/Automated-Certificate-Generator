d9ff7a7515f8d7e0efbf47363f1920b19f24dcfb
Sanjana Jaiswal <jaiswalsanjana0@gmail.com>
2020-07-14 01:31:36 +0530

    Updated script

--- select_cood.py
+++ select_cood.py
@@ -6,18 +6,19 @@ f = open("coords.txt","w")
 def draw_circle(event,x,y,flags,param):
     if event == cv.EVENT_LBUTTONDBLCLK:
         #img[:] = 0
-        cv.putText(img,"coordinates (%d,%d)"%(x,y),(60,60),2,1,(0,255,0)) #SELECT LOCATION OF TEXT(TRIAL & ERROR)
-        f.write(str(x)+"\n")                                              #SELECT TEXT'S TOP SIDE COORDS 
-        f.write(str(y)+"\n")                                              #DOUBLE CLICK TO SELECT
-                                                                           #COORDS WITH GREEN TEXT WILL BE DISPLAYED
+        cv.putText(img,"coordinates (%d,%d)"%(x,y),(60,60),2,1,(0,255,0)) 
+        f.write(str(x)+"\n")                                              
+        f.write(str(y)+"\n")                                              
+                                                                          
 # Create a black image, a window and bind the function to window
 img = cv.imread("certificate.jpg")
 
 
 #cv.imshow('image',img)
 cv.namedWindow('image')
-cv.setMouseCallback('image',draw_circle)
+
 while(1):
+    cv.setMouseCallback('image',draw_circle)
     cv.imshow('image',img)
     if cv.waitKey(10) & 0xFF==27:   #Press Escape Key to terminate window
         break
